649ab0678356fb5e83e0016afcbb9e205d0181a6
0KoIchI0 <dmitryyzeleznov2413@gmail.com>
2025-05-06 17:46:10 +0300

    les_4 update 1

--- main.py
+++ main.py
@@ -35,7 +35,12 @@ class Student:
     def live(self, day):
         day = "Day" + str(day) + "of" +self.name + "life"
         print(f"{day:=^50}")
-        live_cube = random.randint(1, 3)
+        if self.gladness >= 50:
+            live_cube = 1
+        elif self.progress<=0:
+            live_cube =1
+        else :
+            live_cube = random.randint(1, 3)
         if live_cube == 1:
             self.to_study()
         elif live_cube == 2:
